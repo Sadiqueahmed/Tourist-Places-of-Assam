09300d15c4d7c6fad63678ec858be16ce26615d8
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-07-16 17:34:06 +0530

    updated

--- scipts/index.js
+++ scipts/index.js
@@ -50,14 +50,7 @@ var swiper = new Swiper(".home-slider", {
     },
 });
 
-function scrollToNextSection() {
-    const currentSection = document.activeElement.closest('section');
-    const nextSection = currentSection.nextElementSibling;
-  
-    if (nextSection) {
-      nextSection.scrollIntoView();
-    }
-  }
+
 
 
 
