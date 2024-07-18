83997fb2613f83aff18c07269893a4cda8ba5ed9
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-07-18 09:18:20 +0530

    Update index.js

--- scipts/index.js
+++ scipts/index.js
@@ -328,4 +328,14 @@ sortByName.addEventListener("change", (e) => {
   }
 })
 
+// -------------------------------------------to top scroller------------------------------------
 
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
