814aa92028409a80cd71c8ee561721b540d23b42
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-07-24 12:40:52 +0530

    .

--- assets/locations details/location.js
+++ assets/locations details/location.js
@@ -10,3 +10,21 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
