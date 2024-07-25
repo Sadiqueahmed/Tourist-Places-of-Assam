673cc2e8d2d7b6077b91bbdeb2eda93d2d6f437e
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-07-25 20:52:01 +0530

    Update location.js

--- locations details/location.js
+++ locations details/location.js
@@ -203,7 +203,25 @@ window.addEventListener("scroll", () => {
 
 
 
-
+var swiper = new Swiper(".clients-slider", {
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+  },
+});
 
 
 
