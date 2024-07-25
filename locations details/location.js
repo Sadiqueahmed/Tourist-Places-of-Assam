66f81254d96b3a35bb6749af6b0fa62933430068
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-07-25 19:56:06 +0530

    .

--- locations details/location.js
+++ locations details/location.js
@@ -187,7 +187,17 @@ slider();
 
 
 
+// -------------------------------------------to top scroller------------------------------------
 
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
 
 
 
