b43ec2b14097d85c464f3ac14e621fbbad18bb94
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-07-27 11:41:18 +0530

    /

--- locations details/location.js
+++ locations details/location.js
@@ -57,7 +57,23 @@ const tabsContent = document.querySelectorAll('.operations__content');
 
 
 
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
 
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
 
 // Tabbed component
