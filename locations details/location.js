e7a081f9e1c2efa2a11ea6ba3943d7cd79161d36
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-07-28 11:24:47 +0530

    /

--- locations details/location.js
+++ locations details/location.js
@@ -328,24 +328,4 @@ var swiper = new Swiper(".clients-slider", {
 
 
 
-// -----------------------------------------------------------------review slider---------------------------------------
-var swiper = new Swiper(".review-slider", {
-  loop:true, 
-  grabCursor:true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      640: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-  },
-});
+
