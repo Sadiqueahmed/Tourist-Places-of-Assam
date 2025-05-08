9dc7f9fa8f82851beec02ff7edcb15529eed67d5
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-05-08 22:20:59 +0530

    Update index.js

--- scipts/index.js
+++ scipts/index.js
@@ -343,7 +343,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/681cdff43913ed190aa1432f/1iqocll1q';
+    s1.src = 'https://embed.tawk.to/681cdff43913ed190aa1432f';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
